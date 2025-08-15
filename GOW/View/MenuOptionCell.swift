a538823e405e667d30510b10b72d2577106c0386
Rafael <rafael.glez@gmail.com>
2025-08-15 17:24:31 -0600

    Version Inicial

---
+++ GOW/View/MenuOptionCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuOptionCell.swift
+//  GOW
+//
+//  Created by Rafael Gonzalez on 15/08/25.
+//
+
+import UIKit
+
+class MenuOptionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
